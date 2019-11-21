2a74b31b40c335b78094da02d68955ea621d3f97
Ben Dhafer Chayma <35578144+chaymaBD@users.noreply.github.com>
2019-11-21 15:06:57 +0100

    back_end

---
+++ handyman_back_end/src/test/java/project_framework/handyman/HandymanApplicationTests.java
@@ -0,0 +1,13 @@
+package project_framework.handyman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandymanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
