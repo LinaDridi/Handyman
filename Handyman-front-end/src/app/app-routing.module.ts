70c13423aef9696f3d0a9550dc58546786d2da81
LinaDridi <lina.dridi.97@gmail.com>
2019-12-08 01:51:13 +0100

    fixed angular merge problems

--- Handyman-front-end/src/app/app-routing.module.ts
+++ Handyman-front-end/src/app/app-routing.module.ts
@@ -1,37 +1,37 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ArtisansearchComponent } from './components/artisansearch/artisansearch.component';
-import { ArtisanComponent } from './components/artisan/artisan.component';
-import { LoginComponent } from "./components/login/login.component";
-import { RegistrationUserComponent } from "./components/registration-user/registration-user.component";
-import { HomeComponent } from "./components/home/home.component";
-import { RegistrationArtisanComponent } from './components/registration-artisan/registration-artisan.component';
-import { ProjectDescriptionComponent } from './components/project-description/project-description.component';
-import { ArtisanProjectPropositionComponent } from './components/artisan-project-proposition/artisan-project-proposition.component';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AcceptOfferComponent } from './components/accept-offer/accept-offer.component';
+import { ArtisanProjectPropositionComponent } from './components/artisan-project-proposition/artisan-project-proposition.component';
+import { ArtisanComponent } from './components/artisan/artisan.component';
+import { ArtisansearchComponent } from './components/artisansearch/artisansearch.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { DeclineOfferComponent } from './components/decline-offer/decline-offer.component';
-import {SuggestCraftsmanComponent} from './components/suggest-craftsman/suggest-craftsman.component';
-import {ContactUsComponent} from './components/contact-us/contact-us.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProjectDescriptionComponent } from './components/project-description/project-description.component';
+import { RegistrationArtisanComponent } from './components/registration-artisan/registration-artisan.component';
+import { RegistrationUserComponent } from './components/registration-user/registration-user.component';
+import { SuggestCraftsmanComponent } from './components/suggest-craftsman/suggest-craftsman.component';
 
 const routes: Routes = [
 
-  { path: 'SearchArtisan', component: ArtisansearchComponent },
-  { path: 'SearchArtisan/:id', component: ArtisanComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: RegistrationUserComponent },
-  { path: 'signup/artisan', component: RegistrationArtisanComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'project-description', component: ProjectDescriptionComponent },
-  { path: 'artisan/list-project-propositions', component: ArtisanProjectPropositionComponent },
-  { path: 'accept-offer', component: AcceptOfferComponent },
-  { path: 'decline-offer', component: DeclineOfferComponent }
-
-  { path: 'Suggest', component: SuggestCraftsmanComponent},
-  { path: 'contact', component: ContactUsComponent}
+    { path: 'SearchArtisan', component: ArtisansearchComponent },
+    { path: 'SearchArtisan/:id', component: ArtisanComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: RegistrationUserComponent },
+    { path: 'signup/artisan', component: RegistrationArtisanComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'project-description', component: ProjectDescriptionComponent },
+    { path: 'artisan/list-project-propositions', component: ArtisanProjectPropositionComponent },
+    { path: 'accept-offer', component: AcceptOfferComponent },
+    { path: 'decline-offer', component: DeclineOfferComponent },
+    { path: 'Suggest', component: SuggestCraftsmanComponent },
+    { path: 'contact', component: ContactUsComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Handyman-front-end/src/app/components/accept-offer/accept-offer.component.ts
+++ Handyman-front-end/src/app/components/accept-offer/accept-offer.component.ts
@@ -1,30 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material';
+import { MatDialogRef } from '@angular/material';
+import { Router } from '@angular/router';
 
-import { RouterModule, Router } from '@angular/router';
-import { ArtisanService } from '../../services/artisan.service'
+import { ArtisanService } from '../../services/artisan.service';
+
 @Component({
-  selector: 'app-accept-offer',
-  templateUrl: './accept-offer.component.html',
-  styleUrls: ['./accept-offer.component.scss']
+    selector: 'app-accept-offer',
+    templateUrl: './accept-offer.component.html',
+    styleUrls: ['./accept-offer.component.scss']
 })
 export class AcceptOfferComponent implements OnInit {
-  form: any = {};
-  isLoggedIn = false;
-  isLoginFailed = false;
-  errorMessage = '';
-  roles: string[] = [];
+    form: any = {};
+    isLoggedIn = false;
+    isLoginFailed = false;
+    errorMessage = '';
+    roles: string[] = [];
 
-  //still send idproject from component to component
-  constructor(private dialogRef: MatDialogRef<AcceptOfferComponent>, private router: Router, private artisanService: ArtisanService) { }
-  ngOnInit() {
+    // still send idproject from component to component
+    constructor(private dialogRef: MatDialogRef<AcceptOfferComponent>, private router: Router, private artisanService: ArtisanService) { }
+    ngOnInit() {
 
-  }
-  onSubmit(f) {
-    console.log("hellooo")
-    console.log(f.value)
-    this.artisanService.acceptOffer(1, f.value.cost, f.value.currency).subscribe();
-  }
+    }
+    onSubmit(f) {
+        console.log('hellooo');
+        console.log(f.value);
+        this.artisanService.acceptOffer(1, f.value.cost, f.value.currency).subscribe();
+    }
 }
 
 
